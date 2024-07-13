55984b9b84d06f9f4b8da09e5018a06280fa6ca7
Alp Emre Elmas <elmasalpemre@gmail.com>
2024-07-13 08:41:03 +0200

    fix: change drawer color & floating header bg color

--- src/components/drawer.js
+++ src/components/drawer.js
@@ -6,7 +6,7 @@ import { Button } from '@/components/ui/button.jsx'
 
 export function MobileDrawer() {
   return (
-    <Drawer.Root shouldScaleBackground>
+    <Drawer.Root shouldScaleBackground >
       <Button variant="ghost" size="icon" title="Toggle drawer" asChild>
         <Drawer.Trigger>
           <CommandIcon size={16} />
@@ -14,8 +14,8 @@ export function MobileDrawer() {
       </Button>
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-        <Drawer.Content className="fixed bottom-0 left-0 right-0 mt-24 flex h-[80%] flex-col rounded-t-lg bg-gray-100">
-          <div className="flex-1 overflow-y-auto rounded-t-[10px] bg-white p-4">
+        <Drawer.Content className="fixed bottom-0 left-0 right-0 mt-24 flex h-[80%] flex-col rounded-t-lg bg-gray-100 dark:bg-dark-bg-side">
+          <div className="flex-1 overflow-y-auto rounded-t-[10px] bg-white p-4 dark:bg-dark-bg-side">
             <div className="mx-auto mb-8 h-1.5 w-12 flex-shrink-0 rounded-full bg-zinc-300" />
             <MenuContent />
           </div>
